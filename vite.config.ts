370867368896644cfc0cd7291214adceae925a15
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2025-02-02 20:02:48 +0700

    add: core API class instance

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "./src/common/components"),
+      "@types": path.resolve(__dirname, "./src/common/types/index.ts"),
+      "@utils": path.resolve(__dirname, "./src/common/utils"),
     },
   },
 });
