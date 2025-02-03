d730d775a62884d1e21ad193d2dd496189306df6
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2025-02-03 14:00:52 +0700

    add: ntc section - edit some shadow

--- src/pages/NTC/sections/1-Home.tsx
+++ src/pages/NTC/sections/1-Home.tsx
@@ -4,10 +4,10 @@ const HomeSection = () => {
   return (
     <div className="h-full w-full pt-20 pb-16 px-24">
       {/* component: content carousel card */}
-      <div className="w-full h-full rounded-lg relative bg-black text-white">
+      <div className="w-full h-full relative text-white">
         <div className="grid grid-rows-[1fr_auto] grid-cols-5 h-full">
           {/* text: image description */}
-          <div className="col-span-2 bg-blue-500 flex flex-col pt-16 ps-10 pe-4">
+          <div className="col-span-2 bg-black flex flex-col pt-16 ps-10 pe-4 rounded-tl-3xl">
             <h1 className="text-3xl border">TITLE</h1>
             <p className="text-lg h-full border">Description</p>
             <div className="h-16 border pt-2">
@@ -22,9 +22,13 @@ const HomeSection = () => {
               src={placeholderImage}
             />
 
-            {/* style: black gradient that mask images */}
+            {/* style: black gradient that mask images (left) */}
             <div className="absolute top-0 left-0 w-1/2 h-full bg-gradient-to-l from-transparent to-black/80" />
-            <div className="absolute bottom-0 left-0 w-full h-1/4 bg-gradient-to-t from-black/50 to-transparent" />
+            <div className="absolute top-0 left-0 w-1/3 h-full bg-gradient-to-l from-transparent to-black/50" />
+
+            {/* style: black gradient that mask images (bottom) */}
+            <div className="absolute bottom-0 left-0 w-full h-1/4 bg-gradient-to-t from-black/80 to-transparent" />
+            <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-black/50 to-transparent" />
 
             <div className="absolute bottom-3 right-6">
               <div className="border">Carousel Button</div>
@@ -32,7 +36,7 @@ const HomeSection = () => {
           </div>
 
           {/* style: bottom black bar */}
-          <div className="col-span-5 bg-red-500 h-16">empty black bar</div>
+          <div className="col-span-5 bg-black h-16 rounded-b-2xl" />
         </div>
       </div>
     </div>
