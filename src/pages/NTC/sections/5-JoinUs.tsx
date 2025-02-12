5850e6f0909feeba26204c507eb58a9eca49c5ee
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2025-02-12 20:59:29 +0700

    enhance: ntc sect. 5
    - better screen zoom adjustment
    - some icons

--- src/pages/NTC/sections/5-JoinUs.tsx
+++ src/pages/NTC/sections/5-JoinUs.tsx
@@ -1,33 +1,39 @@
 import placeholderImage from "@assets/content/placeholder.jpg";
+import { movingArrow } from "@classStyle/hover";
+import clsx from "clsx";
 import moment from "moment";
+import { FaArrowRight } from "react-icons/fa";
 
 const JoinUsSection = () => {
   return (
-    <div className="h-full w-full">
-      <div className="h-full flex flex-col">
+    <div className="h-screen w-full relative flex items-center justify-center overflow-hidden">
+      <div className="w-full max-w-[100rem] h-full flex flex-col">
         {/* text: title */}
-        <div className="h-[35%] px-44 flex">
-          <div className="w-full mt-auto pb-24 flex">
-            <span className="text-[3rem] font-bold border h-20 w-[36rem]">
+        <div className="h-[28%] flex">
+          <div className="w-full mt-auto mb-12 flex">
+            <span className="text-[3rem] font-bold h-20 w-[36rem]">
               JOIN US!
             </span>
           </div>
         </div>
 
         {/* component: community card */}
-        <div className="h-[45%] flex gap-8 px-44 border">
-          <CommunityCard />
-          <CommunityCard />
+
+        <div className="h-[55%] gap-4 relative">
+          <div className="flex items-center gap-8 h-[32.5rem]">
+            <CommunityCard />
+            <CommunityCard />
+          </div>
         </div>
 
         {/* component: divider */}
-        <div className="h-[7%] gap-3 px-44 flex">
-          <hr className="border w-full mt-auto" />
+        <div className="h-[5%] gap-3 flex">
+          <hr className="w-full mt-auto border-t-2 border-black" />
         </div>
 
         {/* component: footer section */}
-        <div className="h-[13%] px-44 flex items-center">
-          <span className="font-semibold border h-fit text-2xl">
+        <div className="h-[12%] flex items-center">
+          <span className="font-semibold h-fit text-xl">
             {`2021 - ${moment.utc().format("YYYY")} © NTC DEPARTMENT, ALL RIGHTS RESERVED`}
           </span>
         </div>
@@ -45,10 +51,10 @@ const CommunityCard = () => {
       />
 
       {/* text: image description */}
-      <div className="absolute bottom-0 left-0 h-28 text-white z-10 border flex">
+      <div className="absolute bottom-0 left-0 h-24 text-white z-10 flex">
         <div className="w-4/5 mx-8 mt-2">
-          <h3 className="text-3xl border w-fit">Title</h3>
-          <p className="text-sm border line-clamp-2">
+          <h3 className="text-[1.4rem] w-fit">Title</h3>
+          <p className="text-[0.7rem] line-clamp-2">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
             quaerat quisquam quidem Lorem ipsum dolor sit amet consectetur
             adipisicing elit. Nesciunt quaerat quisquam quidem
@@ -56,8 +62,10 @@ const CommunityCard = () => {
         </div>
 
         {/* button: arrow right > redirect to content */}
-        <div className="w-1/5 border flex items-center justify-center">
-          <button className="text-3xl border">{"->"}</button>
+        <div className="w-1/5 flex items-center justify-center">
+          <button className="group py-2 px-4 cursor-pointer">
+            <FaArrowRight className={clsx("w-5 h-5", movingArrow)} />
+          </button>
         </div>
       </div>
 
@@ -65,6 +73,7 @@ const CommunityCard = () => {
       <div className="absolute bottom-4 left-0 w-full h-1/3 bg-gradient-to-t from-black/70 to-transparent rounded-b-lg" />
       <div className="absolute bottom-4 left-0 w-full h-1/2 bg-gradient-to-t from-black/85 to-transparent rounded-b-lg" />
       <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black/95 to-transparent rounded-b-lg" />
+      <div className="absolute bottom-0 left-0 w-full h-5 bg-black rounded-b-lg" />
     </div>
   );
 };
